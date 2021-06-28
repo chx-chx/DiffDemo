e88e7029fbda05ffc2a95e8fb455288e16469125
chx <782876280@qq.com>
2021-06-28 22:13:27 +0800

    pytest

---
+++ test_Diffdemo.py
@@ -0,0 +1,30 @@
+# test.py
+# coding:utf-8
+
+import unittest
+
+
+def add_numb(a, b):
+    return a + b
+
+
+def division_numb(a, b):
+    return a / b
+
+
+class Test(unittest.TestCase):
+    def test_add_1(self):
+        self.assertEqual(add_numb(1, 1), 2)
+
+    def test_add_2(self):
+        self.assertEqual(add_numb(2, 0), 1)
+
+    # def test_division_1(self):
+    #     self.assertEqual(division_numb(2, 1), 2)
+    #
+    # def test_division_2(self):
+    #     self.assertEqual(division_numb(2, 0), 2)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
